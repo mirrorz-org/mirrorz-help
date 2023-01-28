7a225976f4988715766ad6315f82cf067f51da48
SukkaW <isukkaw@gmail.com>
2023-01-29 01:19:54 +0800

    fix(meta): add a fallback to last update

--- src/lib/server/get-last-updated-timestamp.ts
+++ src/lib/server/get-last-updated-timestamp.ts
@@ -1,10 +1,16 @@
 import { Repository } from '@napi-rs/simple-git';
 import path from 'path';
 
+const fallback = Date.now();
+
 let repo: Repository | undefined;
 const rootPath = process.cwd();
 
 export const getLastUpdatedTimestamp = async (file: string): Promise<number> => {
   repo ||= new Repository(rootPath);
-  return repo.getFileLatestModifiedDateAsync(path.relative(rootPath, file));
+  try {
+    return repo.getFileLatestModifiedDateAsync(path.relative(rootPath, file));
+  } catch {
+    return fallback;
+  }
 };

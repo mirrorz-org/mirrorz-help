a5076f318208164bd7d0ee6fb9cd5d9ad50fbcdd
SukkaW <isukkaw@gmail.com>
2023-03-02 04:01:55 +0800

    fix: remove workaround for react issue (upgrading react@next)
    https://github.com/facebook/react/issues/25994#issuecomment-1450685077
    https://github.com/facebook/react/pull/26275

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,56 +14,6 @@ export default function MyDocument() {
     } catch (e) {}
   })(localStorage, document)
         */}
-        {process.env.NODE_ENV !== 'production' && <script
-          dangerouslySetInnerHTML={{
-            __html: `
-/**
- * !!WARNING!!
- * TEMPORARILY WORKAROUND A REACT DEVTOOLS ISSUE https://github.com/facebook/react/issues/25994
- * REMOVE AFTER THE ISSUE IS FIXED
- */
-// Save the original __REACT_DEVTOOLS_GLOBAL_HOOK__.inject
-const reactDevToolsHookInject = window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject;
-// Override the original __REACT_DEVTOOLS_GLOBAL_HOOK__.inject
-// This will allow us to intercept and modify incoming injectProfilingHooks
-window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function inject(...args) {
-  const newArgs = args.map(arg => {
-    // Only modify the original arguments when injectProfilingHooks is present
-    if (!arg || !arg.injectProfilingHooks) return arg;
-
-    const { injectProfilingHooks: originalInjectProfilingHooks, ...rest } = arg;
-    return {
-      // Override the original injectProfilingHooks
-      // This will allow us to intercept and modify incoming hooks
-      injectProfilingHooks(...hooks) {
-        const newHooks = hooks.map(hook => {
-          // Only modify the original hooks when markComponentSuspended is present
-          if (!hook || !hook.markComponentSuspended) return hook;
-
-          // Override the original markComponentSuspended from the hook
-          const { markComponentSuspended: orignalMarkComponentSuspended, ...rest2 } = hook;
-          return {
-            markComponentSuspended(fiber, wakeable, lanes) {
-              if (typeof wakeable.then === 'function') {
-                return orignalMarkComponentSuspended.call(this, fiber, wakeable, lanes);
-              } else {
-                // If "wakeable.then" is not a function, log a warning.
-                console.warn('React DevTools issue detected and mitigated!\\nSee https://github.com/facebook/react/issues/25994 for more information.', { fiber, wakeable, lanes });
-              }
-            },
-            ...rest2
-          };
-        });
-        originalInjectProfilingHooks.apply(this, newHooks);
-      },
-      ...rest
-    };
-  });
-  return reactDevToolsHookInject.apply(this, newArgs);
-};
-            `
-          }}
-        />}
         <script dangerouslySetInnerHTML={{ __html: '!function(b,c){try{var a=b["user-color-scheme"];a&&c.classList.add(a)}catch(d){}}(localStorage,document.documentElement)' }} />
       </Head>
       <body>

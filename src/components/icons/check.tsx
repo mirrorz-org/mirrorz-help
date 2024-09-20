a74e6a9a0ca3391ed09a4ee8dfe0c4923ccd78c7
SukkaW <isukkaw@gmail.com>
2024-09-20 21:15:44 +0800

    chore: make eslint happy

--- src/components/icons/check.tsx
+++ src/components/icons/check.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 
-function IconCheck(p: JSX.IntrinsicElements['svg']) {
+function IconCheck(p: React.JSX.IntrinsicElements['svg']) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...p}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />

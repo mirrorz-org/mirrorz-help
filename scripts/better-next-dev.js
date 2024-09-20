04bcca34a16d35f6a1d98b9095ed5bc8cb5e685d
SukkaW <isukkaw@gmail.com>
2024-09-20 21:22:46 +0800

    chore: replace `chokidar` with `@parcel/watcher`

--- scripts/better-next-dev.js
+++ scripts/better-next-dev.js
@@ -3,11 +3,12 @@
 // Modified from next-remote-watch
 // https://github.com/hashicorp/next-remote-watch/blob/6a56b7fc42d4f354ed6da61626074ac51d1c2783/bin/next-remote-watch#L1
 const picocolors = require('next/dist/lib/picocolors');
-const chokidar = require('chokidar');
+const watcher = require('@parcel/watcher');
 const express = require('express');
 const next = require('next');
 const { parse } = require('node:url');
 const Log = require('next/dist/build/output/log');
+const path = require('node:path');
 
 const port = Number.parseInt(process.env.PORT, 10) || 3000;
 const hostname = process.env.HOSTNAME || 'localhost';
@@ -24,17 +25,14 @@ const handle = app.getRequestHandler();
 (async () => {
   await app.prepare();
 
-  chokidar.watch('./contents', { usePolling: false }).on(
-    'change',
-    () => {
-      // Instead of reloading page, we patch this to only HMR the content pages
-      app.server.hotReloader.send('building');
-      app.server.hotReloader.send({
-        event: 'serverOnlyChanges',
-        pages: ['/[...content]']
-      });
-    }
-  );
+  await watcher.subscribe(path.join(__dirname, '../contents'), () => {
+    // Instead of reloading page, we patch this to only HMR the content pages
+    app.server.hotReloader.send('building');
+    app.server.hotReloader.send({
+      event: 'serverOnlyChanges',
+      pages: ['/[...content]']
+    });
+  });
 
   // create an express server
   const server = express();
@@ -65,5 +63,9 @@ const handle = app.getRequestHandler();
   server.listen(port, (err) => {
     if (err) throw err;
     Log.ready(`started server http://${hostname}:${port}`);
+  });
+
+  process.on('beforeExit', () => {
+    watcher.unsubscribe();
   });
 })();

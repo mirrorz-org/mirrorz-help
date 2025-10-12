0b6ad23aa55d566e21fe4f21334ee115f6efae82
SukkaW <isukkaw@gmail.com>
2025-10-13 04:40:43 +0800

    chore: replace style9

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const withStyle9 = require('style9-webpack/next');
+const withStyleX = require('stylex-webpack/next');
 const withBundleAnalyzer = process.env.ANALYZE === 'true'
   ? require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true'
@@ -8,9 +8,8 @@ const withBundleAnalyzer = process.env.ANALYZE === 'true'
   : (x) => x;
 
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer(withStyle9({
-  incrementalClassnames: false,
-  minifyProperties: process.env.NODE_ENV === 'production'
+module.exports = withBundleAnalyzer(withStyleX({
+  nextjsAppRouterMode: false
 })({
   trailingSlash: true,
   reactStrictMode: true,

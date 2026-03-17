mirrorz-org/mirrorz-help

commit 368b55048b69fee89f7660cabc74291bfec0838b

New zhelp new format (#249)
* [WIP] allow bool menus

* menus: regenerate result for each changed choice

* add page-level global menu

* ECodeBlock: fix URL parsing error when mirrorURL is not ready

* add inline template

* Toc: fix for headings with nested markups

* [WIP] Integrate mirrorz-docs

* highlight: register plaintext module

* [WIP] add text input

* QuickConf: use printf instead of echo and fix escape for quote

* [WIP] Initialize page global variables from parent

* [WIP] Do not interleave choices from multiple global menu sets

* highlight: register console and gradle

* [WIP] Prevent the use of new Function()

* [WIP] Dedupe templates

src/components/mdx-components/enhanced-codeblock/build-code.ts
@@ -1,13 +1,4 @@
-import { escapeStringRegexp } from 'next/dist/shared/lib/escape-regexp';
-
-export function buildCode(code: string, variables: Record<string, string>) {
-  for (const [key, value] of Object.entries(variables)) {
-    code = code.replaceAll(new RegExp(`{{\\s?${escapeStringRegexp(key)}\\s?}}`, 'g'), value);
-  }
-  return code;
-}
-
 export function buildEchoTee(finalCode: string, filePath: string, sudo: boolean) {
-  const escapedFinalCode = finalCode.replaceAll('\'', String.raw`\'`);
-  return `echo '${escapedFinalCode}\n' | ${sudo ? 'sudo ' : ''}tee ${filePath}`;
+  const escapedFinalCode = finalCode.replaceAll('\'', String.raw`'"'"'`);
+  return `printf '%s' '${escapedFinalCode}\n' | ${sudo ? 'sudo ' : ''}tee ${filePath}`;
 }

src/components/mdx-components/enhanced-codeblock/index.tsx
@@ -1,27 +1,27 @@
 import { clsx } from 'clsx';
 import * as stylex from '@stylexjs/stylex';
 
-import { memo, useMemo, useReducer } from 'react';
+import { memo, useState } from 'react';
 import { useSelectedMirror } from '@/contexts/current-selected-mirror';
 import { useMirrorZData } from '@/hooks/use-mirrorz-data';
-import { useCurrentCname } from '@/contexts/current-cname';
 
-import type { Menu, MenuValue } from './menus';
+import type { Menu, MenuValue, InputType } from './menus';
 import CodeBlockMenu from './menus';
 import ActualCode from '../codeblock';
 import LoadingOverlay from './overlay';
 import { TabItem, Tabs } from '../../tabs';
 
-import { buildCode, buildEchoTee } from './build-code';
-import { useMirrorHttpsEnabled } from '@/contexts/mirror-enable-https';
+import { buildEchoTee } from './build-code';
 import { useMirrorSudoEnabled } from '@/contexts/mirror-enable-sudo';
 import { EMPTY_ARRAY } from '../../../lib/client/constant';
 
+import { useRenderCode } from './render-code';
+
 interface CodeBlockProps {
   isHttpProtocol?: boolean,
-  menus?: Menu[],
+  menus?: InputType[],
   children: React.ReactNode,
-  code: string,
+  templateId: string,
   codeLanguage?: string,
   codeMeta?: string,
   enableQuickSetup?: boolean,
@@ -41,16 +41,9 @@ const styles = stylex.create({
   }
 });
 
-function reducer(prevState: Record<string, string>, value: MenuValue) {
-  return {
-    ...prevState,
-    ...value
-  };
-}
-
-function createInitialState(menus: Menu[]): Record<string, string> {
-  return menus.reduce<Record<string, string>>((acc, menu) => {
-    const value = menu.items[0][1];
+function createInitialState(menus: InputType[]): MenuValue {
+  return menus.reduce<MenuValue>((acc, menu) => {
+    const value = 'items' in menu ? menu.items[0][1] : { [menu.name]: menu.defaultValue || '' };
     acc = { ...acc, ...value };
     return acc;
   }, {});
@@ -59,47 +52,22 @@ function createInitialState(menus: Menu[]): Record<string, string> {
 function CodeBlock({
   menus = EMPTY_ARRAY,
   isHttpProtocol = true,
-  code,
+  templateId,
   codeLanguage,
   enableQuickSetup = false,
   filepath
 }: CodeBlockProps) {
-  const [variableState, dispatch] = useReducer(reducer, menus, createInitialState);
+  const [variableState, setVariableState] = useState(() => createInitialState(menus));
 
-  const httpsEnabled = useMirrorHttpsEnabled();
   const sudoEnabled = useMirrorSudoEnabled();
-  const cname = useCurrentCname();
-  const { data, isLoading: _isLoading } = useMirrorZData();
+  const { isLoading: _isLoading } = useMirrorZData();
   const currentSelectedMirror = useSelectedMirror();
 
   const isLoading = _isLoading || !currentSelectedMirror;
 
-  const mirrorUrl = useMemo(() => {
-    if (isLoading) return '(Loading...)';
-    return data?.[0][currentSelectedMirror]?.mirrors[cname].full || '(Loading...)';
-  }, [cname, currentSelectedMirror, data, isLoading]);
+  const finalCode = useRenderCode(templateId, variableState, isHttpProtocol);
 
-  const finalCode = useMemo(() => {
-    const url = new URL('https://' + mirrorUrl);
-    const variable: Record<string, string> = {
-      ...variableState,
-      mirror: mirrorUrl,
-      host: url.host,
-      path: url.pathname,
-      http_protocol: isHttpProtocol ? (httpsEnabled ? 'https://' : 'http://') : '',
-      sudo: sudoEnabled ? 'sudo ' : '',
-      sudoE: sudoEnabled ? 'sudo -E ' : ''
-    };
-    return buildCode(code, variable);
-  }, [code, httpsEnabled, isHttpProtocol, mirrorUrl, sudoEnabled, variableState]);
-
-  /** Validation */
-  if (process.env.NODE_ENV !== 'production' && !code.includes('{{') && !enableQuickSetup) {
-    // eslint-disable-next-line no-console -- log message
-    console.warn('CodeBlock: If you don\' use {{variable}} syntax in your code, and don\'t use "enableQuickSetup", you don\'t have to use <CodeBlock />. The extraneous <CodeBlock /> has code starts with:', code.split('\n')[0]);
-  }
-
-  const codeBlockMenu = menus.length > 0 && <CodeBlockMenu menus={menus} dispatch={dispatch} />;
+  const codeBlockMenu = menus.length > 0 && <CodeBlockMenu menus={menus} dispatch={setVariableState} />;
 
   if (enableQuickSetup && filepath) {
     return (

src/lib/server/parse-markdown.ts
@@ -1,7 +1,6 @@
 import path from 'node:path';
 import fsPromises from 'node:fs/promises';
 import { Buffer } from 'node:buffer';
-import { default as matter } from 'gray-matter';
 import * as metroCache from 'metro-cache';
 
 import routesJson from '@/routes.json';
@@ -20,19 +19,35 @@ import remarkGfm from '@/compiled/remark-gfm';
 import remarkUnwrapImages from '@/compiled/remark-unwrap-images';
 import remarkHeaderCustomId from './remark-header-custom-id';
 import {
-  remarkExtractCodeFromEnhancedCodeBlock,
   remarkProcessNormalCodeBlock
 } from './remark-extract-code-from-codeblock';
+import { stringifyNodeOnServer } from '../shared/react-node-json';
 
 import rehypeExternalLinks from '@/compiled/rehype-external-links';
 
+import ErrnoException = NodeJS.ErrnoException;
+import { parse as yamlParse } from 'yaml';
+import { tokensToMyst } from 'myst-parser';
+import mystPlugin from 'markdown-it-myst';
+import MarkdownIt from 'markdown-it';
+import { VFile } from 'vfile';
+import Hogan from 'hogan.js';
+import * as visitor from 'unist-util-visit';
+import type { Menu, MenuValue, TextInput, InputType, InputCommon } from '@/components/mdx-components/enhanced-codeblock/menus';
+import { toMarkdown } from 'mdast-util-to-markdown';
+import { gfmTableToMarkdown } from 'mdast-util-gfm-table';
+import { mdxToMarkdown } from 'mdast-util-mdx';
+import type { Root as MdastRoot } from 'mdast';
+
+import { renderToStaticMarkup } from 'react-dom/server';
+
 const { FileStore, stableHash } = metroCache;
 
-const contentsPath = path.resolve(process.cwd(), 'contents');
+const docsDir = path.resolve(process.cwd(), 'zdoc');
 
 export interface ToC {
   url: string,
-  text: string,
+  content: string,
   depth: number
 }
 
@@ -40,7 +55,9 @@ export interface ContentProps {
   content: any,
   toc: ToC[],
   meta: MetaFromFrontMatters,
-  cname: string
+  cname: string,
+  compiledTemplates: Record<string, string>,
+  globalVariables?: Record<string, MenuValue>
 }
 
 function fromHrefToSegments(href: string) {
@@ -61,7 +78,7 @@ async function asyncCache<T>(key: string, fn: () => Promise<T>): Promise<T> {
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~ IMPORTANT: BUMP THIS IF YOU CHANGE ANY CODE BELOW ~~~
-const DISK_CACHE_BREAKER = 6;
+const DISK_CACHE_BREAKER = 7;
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const store = new FileStore({
@@ -76,27 +93,348 @@ function fakeRequire(name: string) {
   return name;
 }
 
-export async function getContentBySegments(segments: string[]): Promise<{ props: ContentProps } | { notFound: true }> {
-  const id = segments.length === 0 ? 'index' : (segments.join('/') || 'index');
-  let raw;
-  try {
-    raw = await fsPromises.readFile(path.resolve(contentsPath, `${id}.mdx`), { encoding: 'utf-8' });
-  } catch (e1: any) {
-    if ('code' in e1 && e1.code === 'ENOENT') {
-      try {
-        raw = await fsPromises.readFile(path.resolve(contentsPath, `${id}/index.mdx`), { encoding: 'utf-8' });
-      } catch (e2: any) {
-        if ('code' in e2 && e2.code === 'ENOENT') {
-          Log.warn('[CMS]', id, 'not found');
-          return {
-            notFound: true
-          };
-        }
-        throw e2;
+type pageId = string;
+
+interface ZDocInputCommon {
+  _: string,
+  note?: string
+}
+
+interface ZDocInputOptionSelect {
+  _?: string,
+  [key: string]: string | undefined
+}
+interface ZDocInputOption extends ZDocInputCommon {
+  option: Record<string, ZDocInputOptionSelect>,
+  default?: string
+}
+
+interface ZDocInputBool extends ZDocInputCommon {
+  true?: string | null,
+  false?: string | null,
+  default?: boolean
+}
+
+interface ZDocInputText extends ZDocInputCommon {
+  default?: string
+}
+
+type ZDocInput = ZDocInputOption | ZDocInputBool | ZDocInputText;
+
+interface ZDocConfigOnDisk {
+  _?: string, // Title
+  block?: string[],
+  input?: Record<string, ZDocInput | null>
+};
+
+interface ZDocConfig {
+  name: string,
+  _: string, // Title
+  block: string[],
+  input: Record<string, ZDocInput>
+}
+
+function pathsForPage(page: pageId, file: string) {
+  return {
+    local: path.join(docsDir, 'local', page, file),
+    global: path.join(docsDir, 'global', page, file)
+  };
+}
+
+function isErrnoException(error: any): error is ErrnoException {
+  return error instanceof Error && (error as ErrnoException).code !== undefined;
+}
+
+async function loadFile(page: pageId, file: string, defaultValue?: string): Promise<{ path: string | null, content: string }> {
+  const { local: localPath, global: globalPath } = pathsForPage(page, file);
+
+  for (const p of [localPath, globalPath]) {
+    try {
+      return { path: p, content: await fsPromises.readFile(p, 'utf-8') };
+    } catch (err) {
+      if (!isErrnoException(err) || err.code !== 'ENOENT') {
+        console.error(`Error reading file ${file} for page ${page}: ${(err as Error).message}`);
+        throw err;
       }
     }
-    throw e1;
   }
+  if (defaultValue !== undefined) {
+    console.warn(`file ${file} not found in page ${page}`);
+    return { path: null, content: defaultValue };
+  }
+  console.error(`file ${file} not found in page ${page}`);
+  throw new Error(`file ${file} not found in page ${page}`);
+}
+
+async function loadConf(page: pageId, language: string): Promise<ZDocConfig | null> {
+  const { local: localPath, global: globalPath } = pathsForPage(
+    page,
+    `${language}.yaml`
+  );
+  const tryReadYaml = async (p: string) => {
+    let content: string;
+    try {
+      content = await fsPromises.readFile(p, 'utf-8');
+    } catch (err) {
+      if (!isErrnoException(err) || err.code !== 'ENOENT') {
+        console.error(`Error reading config file ${language}.yaml for page ${page}: ${(err as Error).message}`);
+        throw err;
+      }
+      return [false, null];
+    }
+    return [true, yamlParse(content)];
+  };
+  const [localExists, localConf] = await tryReadYaml(localPath) as [boolean, ZDocConfigOnDisk];
+  const [globalExists, globalConf] = await tryReadYaml(globalPath) as [boolean, ZDocConfigOnDisk];
+  if (!localExists && !globalExists) {
+    console.error(`Config file ${language}.yaml not found for page ${page}`);
+    return null;
+  }
+  const inputsGiven = {
+    ...globalConf?.input,
+    ...localConf?.input
+  };
+  const inputs = Object.fromEntries(Object.entries(inputsGiven).filter(([_, v]) => v !== null));
+  const config = {
+    block: ['index'],
+    ...globalConf,
+    ...localConf,
+    input: inputs as Record<string, ZDocInput>,
+    name: page
+  };
+  if (config._ === undefined) {
+    throw new Error(`Config file ${language}.yaml for page ${page} must have a title field "_"`);
+  }
+  return {
+    ['_']: config._,
+    ...config
+  };
+};
+
+async function loadBlock(page: pageId, block: string, language: string) {
+  return loadFile(page, `${block}.${language}.md`, '');
+};
+
+function transpileInput(name: string, input: ZDocInput): InputType {
+  const items: Menu['items'] = [];
+  const common: InputCommon = {
+    title: input._,
+    note: input.note
+  };
+  if ('option' in input) {
+    const transpileOption = ([optionName, optionSettings]: [string, ZDocInputOptionSelect]) => {
+      const title = optionSettings?._ || optionName;
+      const values = {} as MenuValue;
+      values[name] = optionName;
+      Object.entries(optionSettings || {}).forEach(([k, v]) => {
+        if (k === '_') return;
+        if (v === undefined) return;
+        values[k] = v;
+      });
+      items.push([title, values]);
+    };
+    // If there's a default value and it's valid, put it on the top of the list.
+    if (input.default && input.option[input.default]) {
+      transpileOption([input.default, input.option[input.default]]);
+    }
+    Object.entries(input.option).forEach(([k, v]) => {
+      if (k === input.default) return;
+      transpileOption([k, v]);
+    });
+  } else if ('true' in input || 'false' in input) {
+    const transpileBool = (boolValue: boolean) => {
+      const val = (input)[boolValue ? 'true' : 'false'];
+      items.push([
+        boolValue ? '是' : '否',
+        {
+          [name]: val === null || val === undefined ? boolValue : val
+        }
+      ]);
+    };
+    if (input.default) {
+      transpileBool(true);
+      transpileBool(false);
+    } else {
+      transpileBool(false);
+      transpileBool(true);
+    }
+  } else {
+    return {
+      ...common,
+      defaultValue: (input as ZDocInputText).default,
+      name
+    };
+  }
+  return {
+    ...common,
+    items
+  };
+}
+
+function transpileInputToMenuValue(input: string | null | undefined, inputSettings: ZDocConfig['input']): InputType[] | string {
+  if (!input) return [];
+  const inputNames = input.split(' ');
+  const missingInput = inputNames.find((inputName) => inputSettings[inputName] === undefined);
+  if (missingInput !== undefined) {
+    return missingInput; // The caller will throw an error for this. We return the missing input name to make the error message more specific.
+  }
+  return input.split(' ').map((inputName: string) => {
+    const input = inputSettings[inputName];
+    return transpileInput(inputName, input);
+  });
+}
+
+function createInitialState(menus: InputType[]): MenuValue {
+  return menus.reduce<MenuValue>((acc, menu) => {
+    const value = 'items' in menu ? menu.items[0][1] || {} : { [menu.name]: menu.defaultValue || '' };
+    acc = { ...acc, ...value };
+    return acc;
+  }, {});
+}
+
+function transpileToMdx(blockContent: string, blockPath: string | null, page: pageId, conf: ZDocConfig, globalVariables: Record<string, MenuValue>, generateGlobalMenuId: () => string, templateCompiler: (templateContent: string) => string): string {
+  const tokenizer = new MarkdownIt('commonmark').enable('table');
+  tokenizer.use(mystPlugin);
+  const mdast = tokensToMyst(
+    blockContent,
+    tokenizer.parse(blockContent, {
+      vfile: new VFile(blockPath
+        ? {
+          path: blockPath
+        }
+        : null)
+    })
+  );
+  visitor.visit(mdast, ['mystRole', 'mystDirective', 'mystDirectiveError', 'mystRoleError'],
+    (node) => {
+      if (node.type === 'mystDirectiveError' || node.type === 'mystRoleError') {
+        throw new Error(
+          `Error parsing directive/role on page ${page}, block ${blockPath}: ${(node as any).message} at line ${node.position?.start.line}, column ${node.position?.start.column}`
+        );
+      }
+      if ((node.type === 'mystDirective' || node.type === 'mystRole') && node.name !== 'ztmpl') {
+        throw new Error(
+          `Unsupported directive/role ${node.name} on page ${page}, block ${blockPath} at line ${node.position?.start.line}, column ${node.position?.start.column}`
+        );
+      }
+      if (node.type === 'mystRole') {
+        const roleOptions = {} as Record<string, string | undefined>;
+        node.children?.forEach((child) => {
+          if (child.type === 'mystOption') {
+            roleOptions[child.name] = child.value;
+          }
+        });
+        const templateContent = node.value || '';
+        const templateId = templateCompiler(templateContent);
+        node.type = 'mdxJsxTextElement';
+        node.name = 'CodeInline';
+        node.attributes = [
+          {
+            type: 'mdxJsxAttribute',
+            name: 'codeLanguage',
+            value: roleOptions.lang || undefined
+          },
+          {
+            type: 'mdxJsxAttribute',
+            name: 'templateId',
+            value: templateId
+          }
+        ];
+        node.children = [];
+        return visitor.SKIP;
+      }
+      const directiveOptions = node.options || {};
+      const templateContent = node.value || '';
+      const menus = transpileInputToMenuValue(directiveOptions.input, conf.input);
+      if (typeof menus === 'string') {
+        throw new TypeError(
+          `Input "${menus}" used in directive on page ${page}, block ${blockPath} is not defined in [lang].yaml at line ${node.position?.start.line}, column ${node.position?.start.column}`
+        );
+      }
+      if (directiveOptions.global) {
+        if (!directiveOptions.input) {
+          throw new Error(
+            `Global directive must have input variables on page ${page}, block ${blockPath} at line ${node.position?.start.line}, column ${node.position?.start.column}`
+          );
+        }
+        const menuId = generateGlobalMenuId();
+        globalVariables[menuId] = createInitialState(menus);
+        node.name = 'GlobalMenu';
+        node.attributes = [
+          {
+            type: 'mdxJsxAttribute',
+            name: 'menus',
+            value: {
+              type: 'mdxJsxAttributeValueExpression',
+              value: JSON.stringify(menus)
+            }
+          }, {
+            type: 'mdxJsxAttribute',
+            name: 'id',
+            value: menuId
+          }
+        ];
+      } else {
+        const templateId = templateCompiler(templateContent);
+        node.name = 'CodeBlock';
+        node.attributes = [
+          {
+            type: 'mdxJsxAttribute',
+            name: 'menus',
+            value: {
+              type: 'mdxJsxAttributeValueExpression',
+              value: JSON.stringify(menus)
+            }
+          },
+          {
+            type: 'mdxJsxAttribute',
+            name: 'templateId',
+            value: templateId
+          },
+          ...directiveOptions.lang
+            ? [{
+              type: 'mdxJsxAttribute',
+              name: 'codeLanguage',
+              value: directiveOptions.lang
+            }]
+            : [],
+          ...directiveOptions.path
+            ? [{
+              type: 'mdxJsxAttribute',
+              name: 'filepath',
+              value: directiveOptions.path
+            }, {
+              type: 'mdxJsxAttribute',
+              name: 'enableQuickSetup',
+              value: {
+                type: 'mdxJsxAttributeValueExpression',
+                value: 'true'
+              }
+            }]
+            : []
+        ];
+      }
+      node.type = 'mdxJsxFlowElement';
+      node.children = [];
+      return visitor.SKIP;
+    });
+
+  return toMarkdown(mdast as MdastRoot, { extensions: [mdxToMarkdown(), gfmTableToMarkdown()] });
+}
+
+const language = 'zh';
+
+export async function getContentBySegments(segments: string[]): Promise<{ props: ContentProps } | { notFound: true }> {
+  if (segments.length !== 1) {
+    return { notFound: true };
+  }
+  const id = segments[0];
+  const conf = await loadConf(id, language);
+  if (conf === null) {
+    return { notFound: true };
+  }
+  const blocksNames = conf.block || ['index'];
+  const blocksContent = await Promise.all(blocksNames.map(block => loadBlock(id, block, language)));
 
   const lockfile = await asyncCache('pnpm-lock.yaml', () => fsPromises.readFile(path.join(process.cwd(), 'pnpm-lock.yaml'), { encoding: 'utf-8' }));
   const mdxComponentNames = Object.keys(MDXComponents);
@@ -106,7 +444,7 @@ export async function getContentBySegments(segments: string[]): Promise<{ props:
       // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       // ~~~~ IMPORTANT: Everything that the code below may rely on.
       // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      raw,
+      raw: blocksContent.map(b => b.content).join('\n') + JSON.stringify(conf),
       mdxComponentNames,
       DISK_CACHE_BREAKER,
       lockfile
@@ -126,7 +464,28 @@ export async function getContentBySegments(segments: string[]): Promise<{ props:
     `[CMS] Cache miss for MDX for /${id} from ./node_modules/.cache/`
   );
 
-  const { data: meta, content: mdx } = matter(raw);
+  const globalVariables: Record<string, MenuValue> = {};
+  const compiledTemplates: Record<string, string> = {};
+  const templateDeduplicationMap = new Map<string, string>();
+  let globalMenuCounter = 0;
+  let pageTemplateCounter = 0;
+  const generateGlobalMenuId = () => `globalMenu-${globalMenuCounter++}`;
+  const templateCompiler = (templateContent: string) => {
+    if (templateDeduplicationMap.has(templateContent)) {
+      return templateDeduplicationMap.get(templateContent)!;
+    }
+    const templateId = `template-${pageTemplateCounter++}`;
+    const compiled = Hogan.compile(templateContent, { asString: true });
+    compiledTemplates[templateId] = compiled;
+    templateDeduplicationMap.set(templateContent, templateId);
+    return templateId;
+  };
+  const transpiledBlocks = blocksContent.map(b => transpileToMdx(b.content, b.path, id, conf, globalVariables, generateGlobalMenuId, templateCompiler));
+  const mdx = transpiledBlocks.join('\n\n');
+  const meta: MetaFromFrontMatters = {
+    title: conf._,
+    cname: id
+  };
   // If we don't add these fake imports, the MDX compiler
   // will insert a bunch of opaque components we can't introspect.
   // This will break the prepareMDX() call below.
@@ -136,7 +495,7 @@ export async function getContentBySegments(segments: string[]): Promise<{ props:
 
   const jsxCode = await compileMdx(mdxWithFakeImports, {
     development: false,
-    remarkPlugins: [remarkUnwrapImages, remarkGfm, remarkHeaderCustomId, remarkProcessNormalCodeBlock, remarkExtractCodeFromEnhancedCodeBlock],
+    remarkPlugins: [remarkUnwrapImages, remarkGfm, remarkHeaderCustomId, remarkProcessNormalCodeBlock],
     rehypePlugins: [rehypeExternalLinks]
   });
 
@@ -158,31 +517,15 @@ export async function getContentBySegments(segments: string[]): Promise<{ props:
     props: {
       toc,
       content: JSON.stringify(reactTree, stringifyNodeOnServer),
-      meta: meta as MetaFromFrontMatters,
+      meta,
+      globalVariables,
+      compiledTemplates,
       cname: meta.cname
     }
   };
   // Cache it on the disk.
   await store.set(hash, output);
   return output;
-}
-
-// Serialize a server React tree node to JSON.
-function stringifyNodeOnServer(key: unknown, val: any) {
-  if (
-    val?.$$typeof === Symbol.for('react.transitional.element')
-  ) {
-    // Remove fake MDX props.
-
-    const { mdxType, originalType, parentName, ...cleanProps } = val.props;
-    return [
-      '$r',
-      typeof val.type === 'string' ? val.type : mdxType,
-      val.key,
-      cleanProps
-    ];
-  }
-  return val;
 }
 
 // Get ToC from children
@@ -192,7 +535,7 @@ function getTableOfContents(children: React.ReactNode, depth: number) {
   if (anchors.length > 0) {
     anchors.unshift({
       url: '#',
-      text: 'Overview',
+      content: JSON.stringify('Overview', stringifyNodeOnServer),
       depth: 2
     });
   }
@@ -209,10 +552,10 @@ function extractHeaders(children: React.ReactNode, depth: number, out: ToC[]) {
     if (typeof child === 'object' && 'type' in child && typeof child.type === 'string' && headerTypes.has(child.type)) {
       const cprops = child.props as Record<string, any>;
 
-      const header = {
+      const header: ToC = {
         url: `#${cprops.id}`,
         depth: (child.type && Number.parseInt(child.type.replace('h', ''), 10)) || 0,
-        text: cprops.children
+        content: JSON.stringify(cprops.children, stringifyNodeOnServer)
       };
       out.push(header);
     }

src/components/layout/index.tsx
@@ -1,5 +1,5 @@
 import * as stylex from '@stylexjs/stylex';
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useState } from 'react';
 
 import Nav from './nav';
 import { useRouter } from 'next/router';
@@ -154,7 +154,6 @@ export function Layout({ children, meta, toc = EMPTY_ARRAY, cname, isContent = f
                   <div {...stylex.props(styles.content_wrapper)}>
                     <div {...stylex.props(styles.content_inner)}>
                       {children}
-
                       {isContent && <MetadataCard />}
                     </div>
                   </div>

src/components/mdx-components/globalMenu/index.tsx
@@ -1,0 +1,31 @@
+import { memo } from 'react';
+import type { Menu, MenuValue } from '../enhanced-codeblock/menus';
+import CodeBlockMenu from '../enhanced-codeblock/menus';
+import { EMPTY_ARRAY } from '../../../lib/client/constant';
+import { useSetPageGlobalVariable } from '@/contexts/page-global-variable';
+
+interface GlobalMenuProps {
+  menus?: Menu[],
+  id: string,
+  children: React.ReactNode
+}
+
+function GlobalMenu({
+  menus = EMPTY_ARRAY,
+  id
+}: GlobalMenuProps) {
+  const setPageGlobalVars = useSetPageGlobalVariable();
+  const setVariableState = (value: MenuValue) => {
+    setPageGlobalVars((prev) => ({
+      ...prev,
+      [id]: value
+    }));
+  };
+  return (
+    <div>
+      <CodeBlockMenu menus={menus} dispatch={setVariableState} />
+    </div>
+  );
+}
+
+export default memo(GlobalMenu);

commit bf4d6ead88182a0517eb73eb3314bbf2856cf296

zdoc: add support for attribute append
Fixes: 368b55048b69 ("New zhelp new format (#249)")

src/components/mdx-components/enhanced-codeblock/build-code.ts
@@ -1,4 +1,4 @@
-export function buildEchoTee(finalCode: string, filePath: string, sudo: boolean) {
+export function buildEchoTee(finalCode: string, filePath: string, sudo: boolean, append: boolean) {
   const escapedFinalCode = finalCode.replaceAll('\'', String.raw`'"'"'`);
-  return `printf '%s' '${escapedFinalCode}\n' | ${sudo ? 'sudo ' : ''}tee ${filePath}`;
+  return `printf '%s' '${escapedFinalCode}\n' | ${sudo ? 'sudo ' : ''}tee ${append ? '-a ' : ''}${filePath}`;
 }

src/components/mdx-components/enhanced-codeblock/index.tsx
@@ -25,6 +25,7 @@ interface CodeBlockProps {
   codeLanguage?: string,
   codeMeta?: string,
   enableQuickSetup?: boolean,
+  append?: boolean,
   filepath?: string
 }
 
@@ -55,6 +56,7 @@ function CodeBlock({
   templateId,
   codeLanguage,
   enableQuickSetup = false,
+  append = false,
   filepath
 }: CodeBlockProps) {
   const [variableState, setVariableState] = useState(() => createInitialState(menus));
@@ -84,7 +86,7 @@ function CodeBlock({
           <TabItem value="快速配置" xstyle={styles.code_wrapper}>
             <LoadingOverlay isLoading={isLoading} />
             <ActualCode
-              code={buildEchoTee(finalCode, filepath, sudoEnabled)}
+              code={buildEchoTee(finalCode, filepath, sudoEnabled, append)}
               language="bash"
             />
           </TabItem>

src/lib/server/parse-markdown.ts
@@ -78,7 +78,7 @@ async function asyncCache<T>(key: string, fn: () => Promise<T>): Promise<T> {
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~ IMPORTANT: BUMP THIS IF YOU CHANGE ANY CODE BELOW ~~~
-const DISK_CACHE_BREAKER = 7;
+const DISK_CACHE_BREAKER = 8;
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const store = new FileStore({
@@ -406,6 +406,16 @@ function transpileToMdx(blockContent: string, blockPath: string | null, page: pa
             }, {
               type: 'mdxJsxAttribute',
               name: 'enableQuickSetup',
+              value: {
+                type: 'mdxJsxAttributeValueExpression',
+                value: 'true'
+              }
+            }]
+            : [],
+          ...directiveOptions.append
+            ? [{
+              type: 'mdxJsxAttribute',
+              name: 'append',
               value: {
                 type: 'mdxJsxAttributeValueExpression',
                 value: 'true'
mirrorz-org/mirrorz-help

commit 2652d88aae711249f866fd7e3201d027ffb14c2e

refactor: bool input should use switch/checkbox component (#262)

src/components/mdx-components/enhanced-codeblock/index.tsx
@@ -5,7 +5,7 @@ import { memo, useState } from 'react';
 import { useSelectedMirror } from '@/contexts/current-selected-mirror';
 import { useMirrorZData } from '@/hooks/use-mirrorz-data';
 
-import type { Menu, MenuValue, InputType } from './menus';
+import type { MenuValue, InputType } from './menus';
 import CodeBlockMenu from './menus';
 import ActualCode from '../codeblock';
 import LoadingOverlay from './overlay';
@@ -44,7 +44,11 @@ const styles = stylex.create({
 
 function createInitialState(menus: InputType[]): MenuValue {
   return menus.reduce<MenuValue>((acc, menu) => {
-    const value = 'items' in menu ? menu.items[0][1] : { [menu.name]: menu.defaultValue || '' };
+    const value = 'items' in menu
+      ? menu.items[0][1]
+      : ('trueValue' in menu
+        ? { [menu.name]: menu.defaultValue ? menu.trueValue : menu.falseValue }
+        : { [menu.name]: menu.defaultValue || '' });
     acc = { ...acc, ...value };
     return acc;
   }, {});
@@ -69,7 +73,7 @@ function CodeBlock({
 
   const finalCode = useRenderCode(templateId, variableState, isHttpProtocol);
 
-  const codeBlockMenu = menus.length > 0 && <CodeBlockMenu menus={menus} dispatch={setVariableState} />;
+  const codeBlockMenu = menus.length > 0 && <CodeBlockMenu menus={menus} state={variableState} dispatch={setVariableState} />;
 
   if (enableQuickSetup && filepath) {
     return (

src/components/mdx-components/enhanced-codeblock/menus.tsx
@@ -1,6 +1,7 @@
 import * as stylex from '@stylexjs/stylex';
 import IconChevronUpDown from '../../icons/chevron-up-down';
 import { memo } from 'react';
+import Switch from '../../form/checkbox';
 
 export type MenuValue = Record<string, string | boolean>;
 
@@ -18,7 +19,14 @@ export interface TextInput extends InputCommon {
   defaultValue?: string
 }
 
-export type InputType = Menu | TextInput;
+export interface BooleanInput extends InputCommon {
+  name: string,
+  defaultValue: boolean,
+  trueValue: string | boolean,
+  falseValue: string | boolean
+}
+
+export type InputType = Menu | TextInput | BooleanInput;
 
 const controlBase = {
   display: 'inline-flex',
@@ -98,19 +106,41 @@ const styles = stylex.create({
 
 interface CodeBlockMenuProps {
   menus: InputType[],
+  state: MenuValue,
   dispatch: React.Dispatch<MenuValue>
 }
 
-function CodeBlockMenu({ menus, dispatch }: CodeBlockMenuProps) {
-  const chosenValues = menus.map((entry) => ('items' in entry ? 0 as number : entry.defaultValue || ''));
+function CodeBlockMenu({ menus, state, dispatch }: CodeBlockMenuProps) {
+  const chosenValues = menus.map((entry) => (
+    'items' in entry
+      ? 0 as number
+      : ('trueValue' in entry
+        ? (state[entry.name] === entry.trueValue)
+        : state[entry.name] || entry.defaultValue || '')
+  ));
 
   const handleChange: React.ChangeEventHandler<HTMLSelectElement | HTMLInputElement> = (e) => {
     const { name, value } = e.currentTarget;
     const menuIndex = Number.parseInt(name, 10);
-    const itemValue = 'items' in menus[menuIndex] ? Number.parseInt(value, 10) : value;
+    const menu = menus[menuIndex];
+    let itemValue: number | string;
+    if ('items' in menu) {
+      itemValue = Number.parseInt(value, 10);
+    } else {
+      itemValue = value;
+    }
     chosenValues[menuIndex] = itemValue;
     const newValue = Object.values(chosenValues).reduce<MenuValue>((acc, chosen, menuIndex) => {
-      const value = 'items' in menus[menuIndex] ? menus[menuIndex].items[chosen as number][1] : { [menus[menuIndex].name]: chosen as string };
+      const menu = menus[menuIndex];
+      let value: MenuValue;
+      if ('items' in menu) {
+        value = menu.items[chosen as number][1];
+      } else if ('trueValue' in menu) {
+        const boolVal = chosen as boolean;
+        value = { [menu.name]: boolVal ? menu.trueValue : menu.falseValue };
+      } else {
+        value = { [menu.name]: chosen as string };
+      }
       return {
         ...acc,
         ...value
@@ -119,33 +149,56 @@ function CodeBlockMenu({ menus, dispatch }: CodeBlockMenuProps) {
     dispatch(newValue);
   };
 
+  const handleSwitchChange = (menu: BooleanInput) => () => {
+    const currentValue = state[menu.name] === menu.trueValue;
+    const newBoolValue = !currentValue;
+
+    const newValue = { ...state };
+    newValue[menu.name] = newBoolValue ? menu.trueValue : menu.falseValue;
+
+    dispatch(newValue);
+  };
+
   return (
     <div {...stylex.props(styles.container)}>
       {menus.map((menu, menuIndex) => (
         <div {...stylex.props(styles.menu)} key={menu.title}>
-          <span>{menu.title}</span>
           {'items' in menu
-            ? <div {...stylex.props(styles.input_wrapper)}>
-              <select {...stylex.props(styles.select)} onChange={handleChange} name={menuIndex.toString()} defaultValue="0">
-                {menu.items.map((item, optionIndex) => {
-                  const value = optionIndex.toString();
-                  const key = `${menu.title}_${value}`;
-                  return <option key={key} value={value}>{item[0]}</option>;
-                })}
-              </select>
-              <span {...stylex.props(styles.icon_wrapper)}>
-                <IconChevronUpDown {...stylex.props(styles.icon)} />
-              </span>
-            </div>
-            : <div {...stylex.props(styles.input_wrapper)}>
-              <input
-                {...stylex.props(styles.input_text)}
-                type="text"
-                onChange={handleChange}
-                name={menuIndex.toString()}
-                defaultValue={menu.defaultValue}
-              />
-            </div>}
+            ? <>
+              <span>{menu.title}</span>
+              <div {...stylex.props(styles.input_wrapper)}>
+                <select {...stylex.props(styles.select)} onChange={handleChange} name={menuIndex.toString()} defaultValue="0">
+                  {menu.items.map((item, optionIndex) => {
+                    const value = optionIndex.toString();
+                    const key = `${menu.title}_${value}`;
+                    return <option key={key} value={value}>{item[0]}</option>;
+                  })}
+                </select>
+                <span {...stylex.props(styles.icon_wrapper)}>
+                  <IconChevronUpDown {...stylex.props(styles.icon)} />
+                </span>
+              </div>
+            </>
+            : ('trueValue' in menu
+              ? (
+                <Switch
+                  checked={state[menu.name] === menu.trueValue}
+                  onChange={handleSwitchChange(menu)}
+                  label={menu.title}
+                />
+              )
+              : <>
+                <span>{menu.title}</span>
+                <div {...stylex.props(styles.input_wrapper)}>
+                  <input
+                    {...stylex.props(styles.input_text)}
+                    type="text"
+                    onChange={handleChange}
+                    name={menuIndex.toString()}
+                    defaultValue={menu.defaultValue}
+                  />
+                </div>
+              </>)}
         </div>
       ))}
     </div>

src/components/mdx-components/globalMenu/index.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import type { Menu, MenuValue } from '../enhanced-codeblock/menus';
 import CodeBlockMenu from '../enhanced-codeblock/menus';
 import { EMPTY_ARRAY } from '../../../lib/client/constant';
-import { useSetPageGlobalVariable } from '@/contexts/page-global-variable';
+import { useSetPageGlobalVariable, usePageGlobalVariable } from '@/contexts/page-global-variable';
 
 interface GlobalMenuProps {
   menus?: Menu[],
@@ -14,7 +14,9 @@ function GlobalMenu({
   menus = EMPTY_ARRAY,
   id
 }: GlobalMenuProps) {
+  const pageGlobalVars = usePageGlobalVariable();
   const setPageGlobalVars = useSetPageGlobalVariable();
+  const currentState = pageGlobalVars?.[id] || {};
   const setVariableState = (value: MenuValue) => {
     setPageGlobalVars((prev) => ({
       ...prev,
@@ -23,7 +25,7 @@ function GlobalMenu({
   };
   return (
     <div>
-      <CodeBlockMenu menus={menus} dispatch={setVariableState} />
+      <CodeBlockMenu menus={menus} state={currentState} dispatch={setVariableState} />
     </div>
   );
 }

src/components/form/checkbox.tsx
@@ -71,7 +71,7 @@ export default function Switch({
       <label htmlFor={id} className="mantine-1ld5t9y mantine-Switch-label">
         {label}
       </label>
-      <input onChange={onChange} id={id} type="checkbox" {...stylex.props(styles.sr_only)} />
+      <input checked={checked} onChange={onChange} id={id} type="checkbox" {...stylex.props(styles.sr_only)} />
       <label htmlFor={id} {...stylex.props(styles.track, checked ? styles.track_active : styles.track_inactive)}>
         <div {...stylex.props(styles.thumb, checked && styles.thumb_active)} />
       </label>

src/lib/server/parse-markdown.ts
@@ -28,18 +28,16 @@ import rehypeExternalLinks from '@/compiled/rehype-external-links';
 import ErrnoException = NodeJS.ErrnoException;
 import { parse as yamlParse } from 'yaml';
 import { tokensToMyst } from 'myst-parser';
-import mystPlugin from 'markdown-it-myst';
+import { mystPlugin } from 'markdown-it-myst';
 import MarkdownIt from 'markdown-it';
 import { VFile } from 'vfile';
 import Hogan from 'hogan.js';
 import * as visitor from 'unist-util-visit';
-import type { Menu, MenuValue, TextInput, InputType, InputCommon } from '@/components/mdx-components/enhanced-codeblock/menus';
+import type { Menu, MenuValue, InputType, InputCommon } from '@/components/mdx-components/enhanced-codeblock/menus';
 import { toMarkdown } from 'mdast-util-to-markdown';
 import { gfmTableToMarkdown } from 'mdast-util-gfm-table';
 import { mdxToMarkdown } from 'mdast-util-mdx';
 import type { Root as MdastRoot } from 'mdast';
-
-import { renderToStaticMarkup } from 'react-dom/server';
 
 const { FileStore, stableHash } = metroCache;
 
@@ -242,22 +240,15 @@ function transpileInput(name: string, input: ZDocInput): InputType {
       transpileOption([k, v]);
     });
   } else if ('true' in input || 'false' in input) {
-    const transpileBool = (boolValue: boolean) => {
-      const val = (input)[boolValue ? 'true' : 'false'];
-      items.push([
-        boolValue ? '是' : '否',
-        {
-          [name]: val === null || val === undefined ? boolValue : val
-        }
-      ]);
+    const trueVal = input.true;
+    const falseVal = input.false;
+    return {
+      ...common,
+      name,
+      defaultValue: input.default ?? false,
+      trueValue: trueVal === null || trueVal === undefined ? true : trueVal,
+      falseValue: falseVal === null || falseVal === undefined ? false : falseVal
     };
-    if (input.default) {
-      transpileBool(true);
-      transpileBool(false);
-    } else {
-      transpileBool(false);
-      transpileBool(true);
-    }
   } else {
     return {
       ...common,

commit 2bd38ca423610e98fa82fa73e768c8a116b3cac2

Ecb: fix re-initialization of values when checkbox changed
Fixes: 2652d88aae71 ("refactor: bool input should use switch/checkbox
component")
Fixes: #263

src/components/mdx-components/enhanced-codeblock/index.tsx
@@ -73,7 +73,7 @@ function CodeBlock({
 
   const finalCode = useRenderCode(templateId, variableState, isHttpProtocol);
 
-  const codeBlockMenu = menus.length > 0 && <CodeBlockMenu menus={menus} state={variableState} dispatch={setVariableState} />;
+  const codeBlockMenu = menus.length > 0 && <CodeBlockMenu menus={menus} dispatch={setVariableState} />;
 
   if (enableQuickSetup && filepath) {
     return (

src/components/mdx-components/enhanced-codeblock/menus.tsx
@@ -1,6 +1,6 @@
 import * as stylex from '@stylexjs/stylex';
 import IconChevronUpDown from '../../icons/chevron-up-down';
-import { memo } from 'react';
+import { memo, useState, useEffect } from 'react';
 import Switch from '../../form/checkbox';
 
 export type MenuValue = Record<string, string | boolean>;
@@ -106,30 +106,16 @@ const styles = stylex.create({
 
 interface CodeBlockMenuProps {
   menus: InputType[],
-  state: MenuValue,
   dispatch: React.Dispatch<MenuValue>
 }
 
-function CodeBlockMenu({ menus, state, dispatch }: CodeBlockMenuProps) {
-  const chosenValues = menus.map((entry) => (
+function CodeBlockMenu({ menus, dispatch }: CodeBlockMenuProps) {
+  const [chosenValues, setChosenValues] = useState(menus.map((entry) => (
     'items' in entry
       ? 0 as number
-      : ('trueValue' in entry
-        ? (state[entry.name] === entry.trueValue)
-        : state[entry.name] || entry.defaultValue || '')
-  ));
-
-  const handleChange: React.ChangeEventHandler<HTMLSelectElement | HTMLInputElement> = (e) => {
-    const { name, value } = e.currentTarget;
-    const menuIndex = Number.parseInt(name, 10);
-    const menu = menus[menuIndex];
-    let itemValue: number | string;
-    if ('items' in menu) {
-      itemValue = Number.parseInt(value, 10);
-    } else {
-      itemValue = value;
-    }
-    chosenValues[menuIndex] = itemValue;
+      : ('trueValue' in entry ? entry.defaultValue : entry.defaultValue || '')
+  )));
+  useEffect(() => {
     const newValue = Object.values(chosenValues).reduce<MenuValue>((acc, chosen, menuIndex) => {
       const menu = menus[menuIndex];
       let value: MenuValue;
@@ -147,16 +133,25 @@ function CodeBlockMenu({ menus, state, dispatch }: CodeBlockMenuProps) {
       };
     }, {});
     dispatch(newValue);
+  }, [chosenValues, dispatch, menus]);
+
+  const handleChange: React.ChangeEventHandler<HTMLSelectElement | HTMLInputElement> = (e) => {
+    const { name, value } = e.currentTarget;
+    const menuIndex = Number.parseInt(name, 10);
+    const itemValue = e.currentTarget instanceof HTMLInputElement ? value : Number.parseInt(value, 10);
+    setChosenValues(prev => {
+      const newValues = [...prev];
+      newValues[menuIndex] = itemValue;
+      return newValues;
+    });
   };
 
-  const handleSwitchChange = (menu: BooleanInput) => () => {
-    const currentValue = state[menu.name] === menu.trueValue;
-    const newBoolValue = !currentValue;
-
-    const newValue = { ...state };
-    newValue[menu.name] = newBoolValue ? menu.trueValue : menu.falseValue;
-
-    dispatch(newValue);
+  const handleSwitchChange = (menuIndex: number) => () => {
+    setChosenValues(prev => {
+      const newValues = [...prev];
+      newValues[menuIndex] = !newValues[menuIndex];
+      return newValues;
+    });
   };
 
   return (
@@ -182,8 +177,8 @@ function CodeBlockMenu({ menus, state, dispatch }: CodeBlockMenuProps) {
             : ('trueValue' in menu
               ? (
                 <Switch
-                  checked={state[menu.name] === menu.trueValue}
-                  onChange={handleSwitchChange(menu)}
+                  checked={chosenValues[menuIndex] as boolean}
+                  onChange={handleSwitchChange(menuIndex)}
                   label={menu.title}
                 />
               )

src/components/mdx-components/globalMenu/index.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import type { Menu, MenuValue } from '../enhanced-codeblock/menus';
 import CodeBlockMenu from '../enhanced-codeblock/menus';
 import { EMPTY_ARRAY } from '../../../lib/client/constant';
-import { useSetPageGlobalVariable, usePageGlobalVariable } from '@/contexts/page-global-variable';
+import { useSetPageGlobalVariable } from '@/contexts/page-global-variable';
 
 interface GlobalMenuProps {
   menus?: Menu[],
@@ -14,9 +14,7 @@ function GlobalMenu({
   menus = EMPTY_ARRAY,
   id
 }: GlobalMenuProps) {
-  const pageGlobalVars = usePageGlobalVariable();
   const setPageGlobalVars = useSetPageGlobalVariable();
-  const currentState = pageGlobalVars?.[id] || {};
   const setVariableState = (value: MenuValue) => {
     setPageGlobalVars((prev) => ({
       ...prev,
@@ -25,7 +23,7 @@ function GlobalMenu({
   };
   return (
     <div>
-      <CodeBlockMenu menus={menus} state={currentState} dispatch={setVariableState} />
+      <CodeBlockMenu menus={menus} dispatch={setVariableState} />
     </div>
   );
 }